fe8871c7a233e0df876db9e20b9c72dacf34d5cd
guigro <guigro@gmail.com>
2020-01-07 14:51:27 +0100

    Firt commit with initial files via upload

---
+++ core/class/ggtasks.class.php
@@ -0,0 +1,161 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* * ***************************Includes********************************* */
+require_once __DIR__  . '/../../../../core/php/core.inc.php';
+
+define('__GGTASKS__', dirname(dirname(dirname(__FILE__))));
+require_once (__GGTASKS__.'/3rparty/google-api-php-client-2.4.0/vendor/autoload.php');
+
+
+class ggtasks extends eqLogic {
+    /*     * *************************Attributs****************************** */
+
+
+
+    /*     * *************************CODE POUR RECUPERER LES TACHES****************************** */
+    
+
+
+    /*     * ***********************Methode static*************************** */
+
+    /*
+     * Fonction exécutée automatiquement toutes les minutes par Jeedom
+      public static function cron() {
+
+      }
+     */
+
+
+    /*
+     * Fonction exécutée automatiquement toutes les heures par Jeedom
+      public static function cronHourly() {
+
+      }
+     */
+
+    /*
+     * Fonction exécutée automatiquement tous les jours par Jeedom
+      public static function cronDaily() {
+
+      }
+     */
+
+
+
+    /*     * *********************Méthodes d'instance************************* */
+
+    public function preInsert() {
+        
+    }
+
+    public function postInsert() {
+        
+    }
+
+    public function preSave() {
+        
+    }
+
+    public function postSave() {
+        $info = $this->getCmd(null, 'ggtasks_tasks');
+        if (!is_object($info)) {
+            $info = new ggtasksCmd();
+            $info->setName(__('Tâches', __FILE__));
+        }
+        $info->setLogicalId('ggtasks_tasks');
+        $info->setEqLogic_id($this->getId());
+        $info->setType('info');
+        $info->setSubType('string');
+        $info->save();  
+        
+        $refresh = $this->getCmd(null, 'refresh');
+        if (!is_object($refresh)) {
+            $refresh = new ggtasksCmd();
+            $refresh->setName(__('Rafraichir', __FILE__));
+        }
+        $refresh->setEqLogic_id($this->getId());
+        $refresh->setLogicalId('refresh');
+        $refresh->setType('action');
+        $refresh->setSubType('other');
+        $refresh->save();        
+        
+    }
+
+    public function preUpdate() {
+        
+    }
+
+    public function postUpdate() {
+        
+    }
+
+    public function preRemove() {
+        
+    }
+
+    public function postRemove() {
+        
+    }
+
+    /*
+     * Non obligatoire mais permet de modifier l'affichage du widget si vous en avez besoin
+      public function toHtml($_version = 'dashboard') {
+
+      }
+     */
+
+    /*
+     * Non obligatoire mais ca permet de déclencher une action après modification de variable de configuration
+    public static function postConfig_<Variable>() {
+    }
+     */
+
+    /*
+     * Non obligatoire mais ca permet de déclencher une action avant modification de variable de configuration
+    public static function preConfig_<Variable>() {
+    }
+     */
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+class ggtasksCmd extends cmd {
+    /*     * *************************Attributs****************************** */
+
+
+    /*     * ***********************Methode static*************************** */
+
+
+    /*     * *********************Methode d'instance************************* */
+
+    /*
+     * Non obligatoire permet de demander de ne pas supprimer les commandes même si elles ne sont pas dans la nouvelle configuration de l'équipement envoyé en JS
+      public function dontRemoveCmd() {
+      return true;
+      }
+     */
+
+    public function execute($_options = array()) {
+        
+    }
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+
